584c40f56a09a7765701a8f56441455dcbf2b4cd
Harshada Palanikkumar <hpalanik@ucsc.edu>
2026-02-15 10:28:06 -0800

    DF

---
+++ asgn3/Cube.js
@@ -0,0 +1,64 @@
+class Cube{
+  constructor() {
+    this.type='cube';
+    //this.position = [0.0, 0.0, 0.0];
+    this.color = [1.0, 1.0, 1.0, 1.0];
+   // this.size = 5.0;
+   // this.segments = 10;
+   this.matrix = new Matrix4();
+  }
+
+  render() {
+   // var xy = this.position;
+    var rgba = this.color;
+
+    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
+
+    //Front
+    drawTriangle3DUV( [0,0,0,  1,1,0,  1,0,0], [1,0, 0,1, 1,1 ]);
+    drawTriangle3DUV( [0,0,0,  0,1,0,  1,1,0], [0,0, 0,1, 1,1 ]);
+
+    gl.uniform4f(u_FragColor, rgba[0]*.9, rgba[1]*.9, rgba[2]*.9, rgba[3]);
+
+    // TOP
+    drawTriangle3DUV([0,1,0, 0,1,1, 1,1,1], [0,0, 0,1, 1,1]);
+    drawTriangle3DUV([0,1,0, 1,1,1, 1,1,0], [0,0, 1,1, 1,0]);
+
+    // BACK
+    drawTriangle3DUV([0,0,1, 1,0,1, 1,1,1], [0,0, 1,0, 1,1]);
+    drawTriangle3DUV([0,0,1, 1,1,1, 0,1,1], [0,0, 1,1, 0,1]);
+
+    // BOTTOM
+    drawTriangle3DUV([0,0,0, 1,0,0, 1,0,1], [0,0, 1,0, 1,1]);
+    drawTriangle3DUV([0,0,0, 1,0,1, 0,0,1], [0,0, 1,1, 0,1]);
+
+    // LEFT
+    drawTriangle3DUV([0,0,0, 0,0,1, 0,1,1], [0,0, 1,0, 1,1]);
+    drawTriangle3DUV([0,0,0, 0,1,1, 0,1,0], [0,0, 1,1, 0,1]);
+
+    // RIGHT
+    drawTriangle3DUV([1,0,0, 1,1,0, 1,1,1], [0,0, 0,1, 1,1]);
+    drawTriangle3DUV([1,0,0, 1,1,1, 1,0,1], [0,0, 1,1, 1,0]);
+    //Top
+    //drawTriangle3D( [0,1,0,  0,1,1,  1,1,1]);
+    //drawTriangle3D( [0,1,0,  1,1,1,  1,1,0]);
+
+    //Back
+    // drawTriangle3D( [0,0,1,  1,0,1,  1,1,1]);
+   // drawTriangle3D( [0,0,1,  1,1,1,  0,1,1]);
+
+    //Bottom
+   // drawTriangle3D( [0,0,0,  1,0,0,  1,0,1]);
+    //drawTriangle3D( [0,0,0,  1,0,1,  0,0,1]);
+
+     //Left
+   // drawTriangle3D( [0,0,0,  0,0,1,  0,1,1]);
+    //drawTriangle3D( [0,0,0,  0,1,1,  0,1,0]);
+
+    //right
+   // drawTriangle3D( [1,0,0,  1,1,0,  1,1,1]);
+   // drawTriangle3D( [1,0,0,  1,1,1,  1,0,1]);
+  }
+
+}
